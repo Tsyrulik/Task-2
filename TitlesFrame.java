8f4839ddde7b6eb186f3874453b63559f59761c5
Tsyrulik <47086098+Tsyrulik@users.noreply.github.com>
2019-05-16 01:10:01 +0300

    Добавлена документация

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -1,6 +1,10 @@
 /*
  * Decompiled with CFR 0.139.
  */
+/**
+ * @author Tsyrulyk Serhii
+ * @versin 1.0
+ */
 import java.awt.Component;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -12,8 +16,10 @@ extends JFrame {
     }
 
     private void initUI() {
+        /**Устанавливает заголовок*/
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
+        /**меняет фигуру согласно оператору switch*/
         this.add(new TitlesPanel(31));
         this.setSize(350, 350);
         this.setLocationRelativeTo(null);
